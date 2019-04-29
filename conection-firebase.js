7e3a4a0d42a3cf65b725a0ba4e42c7e0b5882be1
Bruno Fortunato - Help Desk - BNE <brunofortunato@bne.com.br>
2019-04-29 16:15:21 -0300

    correcao form

--- conection-firebase.js
+++ conection-firebase.js
@@ -28,13 +28,61 @@ document.getElementById("form").addEventListener("submit",(e)=>{
 function createTask(taskName,description){
   console.log(counter);
   counter+=1;
-  console.log(counter);
-  var task={
-    id:counter,
-    task: taskName,
-    description:description
-  }
-  let db= firebase.database().ref("tasks/"+counter);
-  db.set(task);
+  $.getJSON("https://jsonip.com?callback=?").then(function(data){
+          var datetime = new Date();
+          var task={
+            id:counter,
+            task: taskName,
+            description:description,
+            ip: data.ip,
+            date: datetime.toLocaleDateString()+" "+datetime.toLocaleTimeString()
+          }
+          console.log(task);
+          let db= firebase.database().ref("teste/"+counter);
+          db.set(task);
+        });
+}
+
+// function DateTime()  {
+//    var s = new Date();
+//    showDateTime = s.toLocaleString()
+//    window.status=showDateTime
+//    document.title=showDateTime
+//    document.getElementById("show").innerHTML="<font face='arial' color='black' size='3'>"+showDateTime+"</font>"
+//    setTimeout("DateTime();", 1000)
+// }
+
+
+// var timestamp = new Date().getTime();
+// myRootRef.set(timestamp);
+//
+// myRootRef.on('value', function(snapshot) {
+//     var currDate = new Date();
+//     var snapshotTimestamp = snapshot.val();
+//
+//     //you can operate on timestamps only...
+//     console.log("Snapshot timestamp: " + snapshotTimestamp);
+//
+//     if(currDate.getTime() >= snapshotTimestamp) {
+//         //do something
+//     }
+//
+//     //...or easily create date object with it
+//     console.log("Snapshot full date: " + new Date(snapshotTimestamp));
+//
+//     if (currDate >=  new Date(snapshotTimestamp)){
+//         //do something
+//     }
+//
+// });
+
+
+
+function getUserIP(){
+  $.getJSON("https://jsonip.com?callback=?", function (data){
+    // console.log(data.ip);
+    return data;
+
+  });
 
 }
