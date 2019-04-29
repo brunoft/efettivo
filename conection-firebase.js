4634095b6515984b5bc57533b6a4ef73b36fccc1
Bruno Fortunato - Help Desk - BNE <brunofortunato@bne.com.br>
2019-04-29 20:40:21 -0300

    form required

--- conection-firebase.js
+++ conection-firebase.js
@@ -43,40 +43,6 @@ function createTask(name,email){
         });
 }
 
-// function DateTime()  {
-//    var s = new Date();
-//    showDateTime = s.toLocaleString()
-//    window.status=showDateTime
-//    document.title=showDateTime
-//    document.getElementById("show").innerHTML="<font face='arial' color='black' size='3'>"+showDateTime+"</font>"
-//    setTimeout("DateTime();", 1000)
-// }
-
-
-// var timestamp = new Date().getTime();
-// myRootRef.set(timestamp);
-//
-// myRootRef.on('value', function(snapshot) {
-//     var currDate = new Date();
-//     var snapshotTimestamp = snapshot.val();
-//
-//     //you can operate on timestamps only...
-//     console.log("Snapshot timestamp: " + snapshotTimestamp);
-//
-//     if(currDate.getTime() >= snapshotTimestamp) {
-//         //do something
-//     }
-//
-//     //...or easily create date object with it
-//     console.log("Snapshot full date: " + new Date(snapshotTimestamp));
-//
-//     if (currDate >=  new Date(snapshotTimestamp)){
-//         //do something
-//     }
-//
-// });
-
-
 
 function getUserIP(){
   $.getJSON("https://jsonip.com?callback=?", function (data){
